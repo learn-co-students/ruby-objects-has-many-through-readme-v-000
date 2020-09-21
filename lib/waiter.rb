32a4d5964f52ce4dbb416081a043db6674dfe055
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-09-21 17:58:03 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,52 @@
+require 'pry'
 class Waiter
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
+
+  def most_freq_customer
+    most_freq_customer_meal = meals.max_by{|meal| meals.count(meal)}
+    most_freq_customer_meal.customer
+  end
+
+  def meal_of_worst_tipper
+    meals.min do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+  end
+
+  def self.tip_avg_lstexp_waiter
+    lstexp_waiter = self.all.min{|waiter_a, waiter_b| waiter_a.yrs_experience <=> waiter_b.yrs_experience}
+    tips_array = lstexp_waiter.meals.map{|meal| meal.tip}
+    tips_array.sum(0.0)/tips_array.size
+  end
+
 
 end
--- spec/01_has_many_through_spec.rb
+++ spec/01_has_many_through_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "Basic class structures" do
-  
+
   before(:example) do
     Customer.class_variable_set(:@@all, [])
     Waiter.class_variable_set(:@@all, [])
@@ -14,12 +14,12 @@ describe "Basic class structures" do
         expect{Customer.new("Ian", 30)}.to_not raise_error
       end
     end
-  
+
     describe ".all" do
       it "is class method that returns the contents of @@all" do
         expect(Customer.all).to eq([])
         ian = Customer.new("Ian", 30)
-      
+
         expect(Customer.all).to eq([ian])
         niky = Customer.new("Niky", 28)
         expect(Customer.all).to eq([ian, niky])
@@ -33,12 +33,12 @@ describe "Basic class structures" do
         expect{Waiter.new("Ian", 3)}.to_not raise_error
       end
     end
-  
+
     describe ".all" do
       it "is class method that returns the contents of @@all" do
         expect(Waiter.all).to eq([])
         ian = Waiter.new("Ian", 3)
-      
+
         expect(Waiter.all).to eq([ian])
         niky = Waiter.new("Niky", 2)
         expect(Waiter.all).to eq([ian, niky])
@@ -89,7 +89,7 @@ describe "Object relationships" do
         howard = Customer.new("Howard", 30)
         terrance = Waiter.new("Terrance", 1)
         howard.new_meal(terrance, 10, 1)
-        
+
         expect(Meal.all.first.waiter).to eq(terrance)
         expect(Meal.all.first.customer).to eq(howard)
       end
@@ -103,13 +103,13 @@ describe "Object relationships" do
         joe = Waiter.new("Joe", 10)
         esmery = Waiter.new("Esmery", 2)
         andrew = Waiter.new("Andrew", 3)
-        
+
         howard.new_meal(terrance, 15, 2)
         howard.new_meal(joe, 15, 4)
         howard.new_meal(andrew, 15, 5)
         daniel.new_meal(terrance, 20, 1)
         daniel.new_meal(esmery, 15, 3)
-        
+
         expect(Meal.all.length).to eq(5)
         expect(howard.meals.length).to eq(3)
         expect(daniel.meals.length).to eq(2)
@@ -129,7 +129,7 @@ describe "Object relationships" do
         joe = Waiter.new("Joe", 10)
         esmery = Waiter.new("Esmery", 2)
         andrew = Waiter.new("Andrew", 3)
-        
+
         howard.new_meal(terrance, 15, 2)
         howard.new_meal(joe, 15, 4)
         howard.new_meal(andrew, 15, 5)
@@ -140,6 +140,19 @@ describe "Object relationships" do
         expect(daniel.waiters).to eq([terrance, esmery])
       end
     end
+
+    describe ".oldest_customer" do
+      it "returns the Customer instance of the oldest customer" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        lisa = Customer.new("Lisa", 27)
+        josh = Customer.new("Josh", 31)
+        steven = Customer.new("Steven", 28)
+
+        expect(Customer.oldest_customer).to eq(josh)
+      end
+    end
+
   end
 
   describe "Waiter" do
@@ -148,7 +161,7 @@ describe "Object relationships" do
         howard = Customer.new("Howard", 30)
         terrance = Waiter.new("Terrance", 1)
         terrance.new_meal(howard, 10, 1)
-        
+
         expect(Meal.all.first.waiter).to eq(terrance)
         expect(Meal.all.first.customer).to eq(howard)
       end
@@ -162,13 +175,13 @@ describe "Object relationships" do
         joe = Waiter.new("Joe", 10)
         esmery = Waiter.new("Esmery", 2)
         andrew = Waiter.new("Andrew", 3)
-        
+
         howard.new_meal(terrance, 15, 2)
         howard.new_meal(joe, 15, 4)
         howard.new_meal(andrew, 15, 5)
         daniel.new_meal(terrance, 20, 1)
         daniel.new_meal(esmery, 15, 3)
-        
+
         expect(terrance.meals.length).to eq(2)
         expect(terrance.meals.first.customer).to eq(howard)
         expect(terrance.meals.last.customer).to eq(daniel)
@@ -183,8 +196,8 @@ describe "Object relationships" do
         josh = Customer.new("Josh", 31)
         steven = Customer.new("Steven", 28)
         terrance = Waiter.new("Terrance", 1)
-        
-        
+
+
         howard.new_meal(terrance, 15, 2)
         daniel.new_meal(terrance, 15, 4)
         lisa.new_meal(terrance, 15, 5)
@@ -194,5 +207,84 @@ describe "Object relationships" do
         expect(terrance.best_tipper).to eq(lisa)
       end
     end
+
+    describe "#most_freq_customer" do
+      it "returns the Customer instance that most frequently interacts with the Waiter instance" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        lisa = Customer.new("Lisa", 27)
+        josh = Customer.new("Josh", 31)
+        steven = Customer.new("Steven", 28)
+        terrance = Waiter.new("Terrance", 1)
+
+
+        howard.new_meal(terrance, 20, 4)
+        howard.new_meal(terrance, 30, 6)
+        howard.new_meal(terrance, 15, 2)
+        daniel.new_meal(terrance, 15, 4)
+        lisa.new_meal(terrance, 15, 5)
+        josh.new_meal(terrance, 15, 1)
+        josh.new_meal(terrance, 45, 3)
+        steven.new_meal(terrance, 15, 3)
+
+        expect(terrance.most_freq_customer).to eq(howard)
+      end
+    end
+
+    describe "#meal_of_worst_tipper" do
+      it "returns the Meal instance associated with this waiter with the worst tip" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        lisa = Customer.new("Lisa", 27)
+        josh = Customer.new("Josh", 31)
+        steven = Customer.new("Steven", 28)
+        terrance = Waiter.new("Terrance", 1)
+
+
+        howard.new_meal(terrance, 20, 4)
+        howard.new_meal(terrance, 30, 6)
+        howard.new_meal(terrance, 15, 2)
+        daniel.new_meal(terrance, 15, 4)
+        lisa.new_meal(terrance, 15, 5)
+        josh.new_meal(terrance, 15, 1)
+        josh.new_meal(terrance, 45, 3)
+        steven.new_meal(terrance, 15, 3)
+
+        expect(terrance.meal_of_worst_tipper.customer).to eq(josh)
+      end
+    end
+
+    describe ".tip_avg_lstexp_waiter" do
+      it "returns the average tip received by the Waiter with the least years of experience" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        lisa = Customer.new("Lisa", 27)
+        josh = Customer.new("Josh", 31)
+        steven = Customer.new("Steven", 28)
+        terrance = Waiter.new("Terrance", 1)
+        jared = Waiter.new("Jared", 4)
+
+
+        howard.new_meal(terrance, 20, 4)
+        howard.new_meal(terrance, 30, 6)
+        howard.new_meal(terrance, 15, 2)
+        daniel.new_meal(terrance, 15, 4)
+        lisa.new_meal(terrance, 15, 5)
+        josh.new_meal(terrance, 15, 1)
+        josh.new_meal(terrance, 45, 3)
+        steven.new_meal(terrance, 15, 3)
+        howard.new_meal(jared, 20, 4)
+        howard.new_meal(jared, 30, 6)
+        howard.new_meal(jared, 15, 2)
+        daniel.new_meal(jared, 15, 4)
+        lisa.new_meal(jared, 15, 5)
+        josh.new_meal(jared, 15, 1)
+        josh.new_meal(jared, 45, 3)
+        steven.new_meal(jared, 15, 3)
+
+        expect(Waiter.tip_avg_lstexp_waiter).to eq(3.5)
+      end
+    end
+
   end
 end
